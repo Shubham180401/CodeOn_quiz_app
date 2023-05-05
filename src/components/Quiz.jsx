972f52ef467dc1bc8ad99c542dba6a7ba6d99603
Shubham <91934830+Shubham180401@users.noreply.github.com>
2023-05-05 14:22:12 +0530

    second chane added some animation

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,16 @@
 import "./css/quiz.css";
 
 import React, { useState } from "react";
+
+// animation npm
+import { motion } from "framer-motion";
+import AnimatedText from "./AnimatedText";
+
 export default function Quiz() {
   const questions = [
     {
-      questionAsk: "What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?",
+      questionAsk:
+        "What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?",
       answerOptions: [
         { optionAns: "false", check: false },
         { optionAns: "false", check: false },
@@ -22,7 +28,8 @@ export default function Quiz() {
       ],
     },
     {
-      questionAsk: "What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?",
+      questionAsk:
+        "What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?What is the right answer?",
       answerOptions: [
         { optionAns: "false", check: false },
         { optionAns: "false", check: false },
@@ -41,6 +48,7 @@ export default function Quiz() {
     },
   ];
 
+  // quiz hooks for the quiz segments
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
@@ -57,33 +65,68 @@ export default function Quiz() {
       setShowScore(true);
     }
   };
+
+  // animation from motiondiv
+  const placeholderText = [
+    { type: "heading1", text: 'You scored ${score} out of ${questions}' },
+  ];
+  const updatedPlaceholderText = [...placeholderText];
+  updatedPlaceholderText[0].text = `You scored ${score} out of ${questions}`;
+
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025,
+      },
+    },
+  };
+
   return (
-    <div className="Quiz">
-      {showScore ? (
-        <div className="score">
-          You scored {score} out of {questions.length}
-        </div>
-      ) : (
-        <>
-          <div className="questions">
-            <div className="question_number">
-              <span>Question {currentQuestion + 1}</span>/{questions.length}
+    <motion.div
+      className="container text-center  bg-black"
+      initial={{ x: 50, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 50, opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <div className="Quiz">
+        {showScore ? (
+          <motion.div
+            //   className="App"
+            initial="hidden"
+            animate="visible"
+            variants={container}
+          >
+            <div className="score">
+              {/* You scored {score} out of {questions.length} */}
+              {updatedPlaceholderText.map((item, index) => {
+                 return <AnimatedText {...item} key={index} />;
+              })}
             </div>
-            <div className="question_asked">
-              {questions[currentQuestion].questionAsk}
+          </motion.div>
+        ) : (
+          <>
+            <div className="questions">
+              <div className="question_number">
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className="question_asked">
+                {questions[currentQuestion].questionAsk}
+              </div>
             </div>
-          </div>
-          <div className="option_menu">
-            {questions[currentQuestion].answerOptions.map((answerOption) => (
-              <button className="options"
-                onClick={() => handleAnswerOptionClick(answerOption.check)}
-              >
-                {answerOption.optionAns}
-              </button>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
+            <div className="option_menu">
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <button
+                  className="options"
+                  onClick={() => handleAnswerOptionClick(answerOption.check)}
+                >
+                  {answerOption.optionAns}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </motion.div>
   );
 }
